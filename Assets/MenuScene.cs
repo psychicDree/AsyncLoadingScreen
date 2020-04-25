c68365d5695c2358dba884d0fa50e2bdaeb55fa8
devmanjha <devmanjha@gmail.com>
2020-04-26 00:47:12 +0530

    Initial commit

---
+++ Assets/MenuScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+    public string loadingSceneName;
+    void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene(loadingSceneName, LoadSceneMode.Additive);
+    }
+
+    void OnQuitButionPressed()
+    {
+        Application.Quit();
+    }
+}
